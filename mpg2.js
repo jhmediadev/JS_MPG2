724c383e18377328b7ec15293a21ad5875f6a9c0
JH <jhmediatech@outlook.com>
2022-11-01 16:35:11 -0400

    Built revised Miles Per Gallon calculator. Updated buttons - Calculate and Form reset.

---
+++ mpg2.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const $ = selector => document.querySelector(selector);
+
+const focusAndSelect = selector => {
+        const elem = $(selector);
+        elem.focus();
+        elem.select();
+}
+
+const getErrorMsg = msg => `${msg} must be a valid number greater than zero.`;
+
+
+const processEntries = () => {
+    const miles = parseFloat($("#milesDriven").value);
+    const pricePerGallon = parseFloat($("#pricePerGallon").value);
+    const vehicleGasRating = parseFloat($("#mpgRating").value);
+    
+
+    if (isNaN(miles) || milesDriven <= 0) {
+        alert(getErrorMsg("Miles driven"));
+        focusAndSelect("#milesDriven");    
+    }
+
+    else if (isNaN(pricePerGallon) || pricePerGallon <= 0) {
+        alert(getErrorMsg("Price per gallon"));
+        focusAndSelect("#pricePerGallon");
+    }
+
+    else if (isNaN(vehicleGasRating) || vehicleGasRating <= 0) {
+        alert(getErrorMsg("Vehicle miles per gallon rating"));
+        focusAndSelect("#mpgRating");
+    }
+
+    else {
+        $("#gasUsed").value = (miles / vehicleGasRating).toFixed(2);
+        $("#totalFuelCost").value = `$ ${((miles / vehicleGasRating) * pricePerGallon).toFixed(2)}`;
+    };
+}
+
+function clearPage() {
+    $("#milesDriven").value = " ";
+    $("#pricePerGallon").value = " ";
+    $("#mpgRating").value = " ";
+    $("#gasUsed").value = " ";
+    $("#totalFuelCost").value = " ";
+    
+    $("#milesDriven").focus();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#calculate").addEventListener("click", processEntries);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#clear").addEventListener("click", clearPage);
+});
